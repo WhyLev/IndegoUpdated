6c017e9196467a573b5c4be532e60a19e7cafb88
kizeu <116279616+kimzeuner@users.noreply.github.com>
2026-06-18 14:29:15 +0200

    Add ENTITY_PREDICTIVE_WEATHER constant

--- custom_components/boschindego/const.py
+++ custom_components/boschindego/const.py
@@ -95,6 +95,7 @@ ENTITY_DELETE_LAST_ALERT_BUTTON: Final = "delete_last_alert"
 ENTITY_READ_ALL_ALERTS_BUTTON: Final = "read_all_alerts"
 ENTITY_READ_LAST_ALERT_BUTTON: Final = "read_last_alert"
 ENTITY_SMARTMOWING_SWITCH: Final = "smartmowing_switch"
+ENTITY_PREDICTIVE_WEATHER: Final = "predictive_weather"
 
 # Smart Mowing Sensors (Phase 1)
 ENTITY_WEATHER: Final = "weather"
